e7b1ea60c24588bb52964ee354f92fe7f79a4b56
LuanCostt <luansenai234bnn@gmail.com>
2025-07-17 08:54:23 -0300

    feat: Conclusão do userController

--- Controller/userController.php
+++ Controller/userController.php
@@ -5,19 +5,58 @@ namespace Controller;
 use Model\User;
 use Exception;
 
-class UserController{
+class UserController
+{
     private $userModel;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->userModel = new User();
     }
-    public function registerUser($user_fullname, $email, $password){
-        try{
+
+    // REGISTRO DE USUÁRIO
+    public function registerUser($user_fullname, $email, $password)
+    {
+        try {
+            if (empty($user_fullname) or empty($email) or empty($password)) {
+                return false;
+            }
+
+            // $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            return $this->userModel->registerUser($user_fullname, $email, $password);
+        } catch (Exception $error) {
+            echo "Erro ao cadastrar usuário: " . $error->getMessage();
+            return false;
         }
-    catch(Exception $error){
-        echo "Erro ao cadastrar usuário: ".$error->getMessage();
+    }
+
+    // LOGIN DE USUÁRIO
+
+    public function checkUserByEmail($email){
+        return $this->userModel->getUserByEmail($email);
+    }
+    public function login($email, $password) {
+        $user = $this->userModel->getUserByEmail($email);
+
+        if($user && password_verify()){
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['user_fullname'] = $user['user_fullname'];
+            $_SESSION['email'] = $user['email'];
+        }
         return false;
     }
+
+    public function isLoggerdIn(){
+        return isset($_SESSION['id']);
+    }
+    // USUÁRIO LOGADO?
+
+    // RESGATAR DADOS DO USUÁRIO
+    public function getUserData($id,$user_fullname, $email){
+        $id = $_SESSION['id'];
+
+        return $this->userModel->getUserInfo($id,$user_fullname,$email);
     }
 }
 
--- Model/User.php
+++ Model/User.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 namespace Model;
 
@@ -9,38 +9,46 @@ use PDOException;
 
 class User {
     private $db;
-
-    /**
-     * Metodo que ira ser executado toda vez que
-     * for criado de um objeto de classe -> USER
+    
+    /*
+     * MÉTODO QUE IRÁ SER EXECUTADO TODA VEZ QUE
+     * FOR CRIADO UM OBJETO DA CLASSE -> USER
      */
+
     public function __construct() {
         $this->db = Connection::getInstance();
     }
-    public function registerUser($user_fullname,$email,$password) {
-        try{
 
-            $sql = 'INSERT INTO user (user_fullname, email, password, created_at) VALUES(:user_fullname, :email, :password, NOW())';
+    // FUNÇÃO DE CRIAR USUÁRIO
+    public function registerUser($user_fullname, $email, $password) {
+        try {
+            // INSERÇÃO DE DADOS NA LINGUAGEM SQL
+            $sql = 'INSERT INTO user (user_fullname, email, password, created_at) VALUES (:user_fullname, :email, :password, NOW())';
 
-            $hashedPassoword = password_hash($password, PASSWORD_DEFAULT);
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-
+            // PREPARAR O BANCO DE DADOS PARA RECEBER O COMANDO ACIMA
             $stmt = $this->db->prepare($sql);
 
+            // REFERENCIAR OS DADOS PASSADOS PELO COMANDO SQL COM OS PARÂMETROS DA FUNÇÃO
             $stmt->bindParam(":user_fullname", $user_fullname, PDO::PARAM_STR);
             $stmt->bindParam(":email", $email, PDO::PARAM_STR);
-            $stmt->bindParam(":password",$hashedPassoword, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $hashedPassword, PDO::PARAM_STR);
+
+            // EXECUTAR TUDO
 
             $stmt->execute();
 
-
-        } catch(PDOException $error) {
-            echo"Erro ao executar o comando".$error->getMessage();
+        } catch (PDOException $error) {
+            // EXIBIR MENSAGEM DE ERRO COMPLETA E PARAR A EXECUÇÃO
+            echo "Erro ao executar o comando " . $error->getMessage();
             return false;
-        } 
+        }
     }
+
+    // LOGIN
     public function getUserByEmail($email) {
-        try{
+        try {
             $sql = "SELECT * FROM user WHERE email = :email LIMIT 1";
 
             $stmt = $this->db->prepare($sql);
@@ -49,10 +57,13 @@ class User {
 
             $stmt->execute();
 
-        }catch(PDOException $error){}
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $error) { }
     }
-    public function getUserInfo($id,$user_fullname, $email) {
-        try{
+
+    // OBTER INFORMAÇÕES DO USUÁRIO
+    public function getUserInfo($id, $user_fullname, $email) {
+        try {
             $sql = "SELECT user_fullname, email FROM user WHERE id = :id AND user_fullname = :user_fullname AND email = :email";
 
             $stmt = $this->db->prepare($sql);
@@ -62,28 +73,30 @@ class User {
             $stmt->bindParam(":email", $email, PDO::PARAM_STR);
 
             $stmt->execute();
+
             /**
              * fetch = querySelector();
+             * fetchAll = querySelectorAll();
              * 
-             * FETCH_ASSOC;
-             * $user{
-             * "user_fullname" =>. "teste",
-             * "email" => "teste@example.com"
-             * }
+             * FETCH_ASSOC:
+             * $user[
+             *  "user_fullname" => "teste",
+             *  "email" => "teste@example.com"
+             * ]
+             * 
+             * 
+             * 
              * COMO OBTER INFORMAÇÕES:
-             * $user["user_fullname"];
+             * $user['user_fullname'];
              */
 
             return $stmt->fetch(PDO::FETCH_ASSOC);
 
-        
-
-        }catch(PDOException $error){
-            echo"Erro ao buscar informações: ".$error->getMessage();
+        } catch (PDOException $error) {
+            echo "Erro ao buscar informações: " . $error->getMessage();
             return false;
         }
     }
-
 }
 
 ?>
